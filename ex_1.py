23dbfcf58cfc9239228865a9969ca9bd20581216
Guszas <pavel.sipaylo23@gmail.com>
2022-09-17 15:10:47 +0300

    Homework2

---
+++ ex_1.py
@@ -0,0 +1,3 @@
+name = input("What ur name:")
+print("Hello " + name)
+
---
+++ ex_2.py
@@ -0,0 +1,3 @@
+name = input("What ur name:")
+second = input("What ur second name:")
+print(" Hello " + name, " Very nice fam name " + second)
