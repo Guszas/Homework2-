42490bb0ade04bfd7cd5963ff81e131b0541c4ff
Guszas <pavel.sipaylo23@gmail.com>
2022-09-26 11:35:22 +0300

    Homework2

--- lessons_3.py
+++ lessons_3.py
@@ -1,3 +1,3 @@
-dict = {"key1": 1, "key2": 2, "key3": 3}  # создаём словарь из 3 элементов
-items = dict.items()  # распаковываем наш словарь по парам
+dict = {"key1": 1, "key2": 2, "key3": 3}
+items = dict.items()
 print(items)
