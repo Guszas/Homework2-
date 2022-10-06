41eebc61ce06fcd7aa8a15688d1257a0f5d419ea
Guszas <112897530+Guszas@users.noreply.github.com>
2022-10-06 20:09:07 +0300

    Delete ex_6.py

--- ex_6.py
+++
@@ -1,4 +0,0 @@
-piece_pizza1 = int(input("How many slices of pizza were there? "))
-piece_pizza2 = int(input("How many slices of pizza did you it? "))
-piece_left = piece_pizza1 - piece_pizza2
-print("You have ", piece_left, " pieces left ")
