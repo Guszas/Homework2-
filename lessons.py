049b02a0382cee6291ec9e12275ed84f45aa1226
Guszas <pavel.sipaylo23@gmail.com>
2022-11-09 13:03:29 +0300

    Homework2

---
+++ lessons.py
@@ -0,0 +1,34 @@
+a = 17 / 2 * 3 + 2
+print(a)
+b = 2 + 17 / 2 * 3
+print(b)
+c = 19 % 4 + 15 / 2 * 3
+print(c)
+d = (15 + 6) - 10 * 4
+print(d)
+e = 17 / 2 % 2 * 4
+print(e)
+
+a = 17 / (2 * 3) + 2
+print(a)
+b = (2 + 17) / 2 * 3
+print(b)
+c = (19 % 4) + 15 / 2 * 3
+print(c)
+d = 15 + (6 - 10) * 4
+print(d)
+e = 17 / 2 % (2 * 4)
+print(e)
+
+vlad = 22
+
+valera = 24
+
+egor = 23
+
+total_age = vlad + valera + egor
+
+print(total_age)
+
+average = 23
+print(average)
